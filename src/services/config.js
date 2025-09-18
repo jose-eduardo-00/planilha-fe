706381e8e3706231b8e999870786ab172cd5c6a3
José Eduardo Gois de Pinho Costa <eduardo@softwareprecisao.com>
2025-09-18 10:13:25 -0300

    feat: ""

--- src/services/config.js
+++ src/services/config.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiUrl = import.meta.env.VITE_API_URL;
-// const apiurl = "http://localhost:3000 "
+// const apiUrl = "http://localhost:3000 "
 
 export const http = axios.create({
   baseURL: apiUrl,
