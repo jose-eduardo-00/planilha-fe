771d9031e927ca97ffcbb4b0dad4d4408d600146
José Eduardo Gois de Pinho Costa <eduardo@softwareprecisao.com>
2025-09-18 10:05:54 -0300

    feat: alteracao de layout verificacao de codigo

--- src/views/auth/verifyCode/VerifyCodeView.vue
+++ src/views/auth/verifyCode/VerifyCodeView.vue
@@ -1,36 +1,20 @@
 <template>
-    <main class="d-flex flex-column align-items-center justify-content-center vh-100 bg-light">
+    <main class="d-flex flex-column align-items-center justify-content-center vh-100 bg-light bg-imagem-cover">
         <MainAlertView v-if="alertVisible" :message="alertMessage" :title="alertTitle" :type="alertType" class="mb-3" />
-        <div class="card shadow-sm p-0 d-flex flex-row" style="width: 60%;">
-            <div class="text-white d-flex flex-column align-items-center justify-content-between px-4"
-                style="width: 50%; background: linear-gradient(180deg, #0d6efd, #66b2ff);">
-                <h4 class="text-center fs-1 fw-semibold mt-5">Verificar Conta</h4>
+        <div class="card px-5 bg-white bg-opacity-25 border border-light border-opacity-25
+            shadow-lg backdrop-blur" style="width: 50%;">
+            <h4 class="text-center fs-1 fw-semibold mt-5">Verificar Conta</h4>
 
-                <div class="" style="margin-top: -5rem;">
-                    <h3 class="text-center fs-4">Bem vindo!</h3>
-
-                    <p class="text-center" style="font-size: 12px;">Esse é um site que serve para você poder gerenciar e
-                        acompanhar suas
-                        planilhas.</p>
-                </div>
-
-                <!-- <div class="d-flex align-items-center justify-content-center mb-4" style="margin-top: -4rem;">
-                    <router-link to="/register" class="fs-6 fw-normal text-white text-decoration-none"
-                        style="cursor: pointer;">
-                        Ainda não possui conta?
-                    </router-link>
-                </div> -->
-                <div></div>
-            </div>
-            <div class="d-flex flex-column px-4 py-5 my-4" style="width: 50%;">
+            <div class="d-flex flex-column px-4 mb-4 mt-3">
                 <div class="mt-4" v-if="step1">
                     <MainInput v-model="email" label="Email" placeholder="seu@email.com" :success="successEmail"
                         :error="errorEmail" />
                 </div>
 
                 <div class="d-flex flex-column align-items-center justify-content-center mb-4 mt-4" v-if="step1">
-                    <MainButton customClass="fw-medium bg-dark border border-dark" text="Enviar" :width="'80%'"
-                        :height="'40px'" :onClick="handleEmail" :isLoading="isLoading" :isDisabled="isLoading" />
+                    <MainButton animationName="zoomIn" customClass="fw-medium bg-dark border border-dark" text="Enviar"
+                        :width="'80%'" :height="'40px'" :onClick="handleEmail" :isLoading="isLoading"
+                        :isDisabled="isLoading" />
                 </div>
 
                 <div class="d-flex align-items-center justify-content-center mt-4" v-if="step2">
@@ -45,13 +29,29 @@
                     Reenviar {{ timer !== 0 ? timer : '' }}</span>
 
                 <div class="d-flex flex-column align-items-center justify-content-center mb-4 mt-3" v-if="step2">
-                    <MainButton customClass="fw-medium bg-dark border border-dark" text="Verificar" :width="'80%'"
-                        :height="'40px'" :onClick="handleCode" :isLoading="isLoadingCode" :isDisabled="isLoadingCode" />
+                    <MainButton animationName="zoomIn" customClass="fw-medium bg-dark border border-dark"
+                        text="Verificar" :width="'80%'" :height="'40px'" :onClick="handleCode"
+                        :isLoading="isLoadingCode" :isDisabled="isLoadingCode" />
                 </div>
             </div>
         </div>
     </main>
 </template>
+
+<style scoped>
+.bg-imagem-cover {
+    background-image: url('../../../../assets/fundo2_planilhas_login.jpg');
+    background-position: center center;
+    background-repeat: no-repeat;
+    background-attachment: fixed;
+    background-size: cover;
+}
+
+.backdrop-blur {
+    backdrop-filter: blur(2px);
+    -webkit-backdrop-filter: blur(2px);
+}
+</style>
 
 <script>
 import MainAlertView from '../../../components/alerts/MainAlertView.vue';
